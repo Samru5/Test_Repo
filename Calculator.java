dc3165a217d8cc72daaad29de5d3f950c8390aee
Shetye <samruddhi-dinesh.shetye@capgemini.com>
2019-03-05 10:27:40 +0530

    Added addition() function

--- Calculator.java
+++ Calculator.java
@@ -5,4 +5,8 @@ public static void main(String[] args)
 System.out.println("Hello World!!!!");
 System.out.println("Calculator Java Program");
 }
+public static void addition(int number1,int number2)
+{
+System.out.println("Addition="+(number1+number2));
+}
 }
