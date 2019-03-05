31649b169f7922278ddacbc0a15bc49978591969
Shetye <samruddhi-dinesh.shetye@capgemini.com>
2019-03-05 10:42:20 +0530

    Added subtraction() function

--- Calculator.java
+++ Calculator.java
@@ -9,4 +9,9 @@ public static void addition(int number1,int number2)
 {
 System.out.println("Addition="+(number1+number2));
 }
+
+public static void subtraction(int number1,int number2)
+{
+System.out.println("Subtraction="+(number1-number2));
+}
 }
